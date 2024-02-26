2b18f5ac227adadaf3aa15eddb2c418d2d70d661
Stefan Schindler <s.schindler@microist.com>
2024-02-26 19:45:36 +0100

    add basic octroprint client

---
+++ octoprint_client/OctoPrintClient.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class OctoPrintClient
+{
+    private readonly string _octoPrintUrl;
+    private readonly string _apiKey;
+    private readonly HttpClient _httpClient;
+
+    public OctoPrintClient(string octoPrintUrl, string apiKey)
+    {
+        _octoPrintUrl = octoPrintUrl;
+        _apiKey = apiKey;
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri(_octoPrintUrl);
+        _httpClient.DefaultRequestHeaders.Add("X-Api-Key", _apiKey);
+    }
+
+    public async Task<string> GetPrinterStatusAsync()
+    {
+        try
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("printer");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                return "Error: " + response.StatusCode;
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            return "HTTP Request Exception: " + e.Message;
+        }
+    }
+}
--- octoprint_client/Program.cs
+++ octoprint_client/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Set your OctoPrint server URL and API key
+        string octoPrintUrl = "http://your_octoprint_server_address/api";
+        string apiKey = "your_api_key";
+
+        // Create an instance of OctoPrintClient
+        OctoPrintClient client = new OctoPrintClient(octoPrintUrl, apiKey);
+
+        // Call the GetPrinterStatusAsync method
+        string printerStatus = await client.GetPrinterStatusAsync();
+
+        // Display the printer status
+        Console.WriteLine("Printer Status:");
+        Console.WriteLine(printerStatus);
+    }
+}
